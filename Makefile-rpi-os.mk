5d280f52cc7cbd28273ceef08dad0a3a0e71b5ff
Innokentii Kozlov <innokentii.kozlov@nokia.com>
2026-03-03 15:09:52 +0200

    update rpi os dir struct output.

--- Makefile-rpi-os.mk
+++ Makefile-rpi-os.mk
@@ -1,7 +1,9 @@
-.PHONY: rpi-os/download
-rpi-os/download: build/container/ansible-runner
+RPI_WOKSPACE_DIR := $(TARGET_DIR)/raspberry-pi
+
+.PHONY: rpi-os/download-files
+rpi-os/download-files: build/container/ansible-runner
 	$(DOCKER_BINARY) run --rm -it \
-		-v $(TARGET_DIR):/workspace \
+		-v $(RPI_WOKSPACE_DIR):/workspace \
 		-v $(ROOT_DIR)/workflows/rpi-os-setup:/playbooks \
 		--user 1000:1000 \
 		-w /workspace \
@@ -9,10 +11,10 @@ rpi-os/download: build/container/ansible-runner
 		ansible-playbook -i /playbooks/inventory/hosts.yaml /playbooks/rpi-os-download.yaml
 
 .PHONY: rpi-os/build
-rpi-os/build: rpi-os/download build/container/ansible-runner
+rpi-os/build: rpi-os/download-files build/container/ansible-runner
 	@rm -rf $(TARGET_DIR)/cloud-init
 	$(DOCKER_BINARY) run --rm -it \
-		-v $(TARGET_DIR):/workspace \
+		-v $(RPI_WOKSPACE_DIR):/workspace \
 		-v $(ROOT_DIR)/workflows/rpi-os-setup:/playbooks \
 		--user 1000:1000 \
 		-w /workspace \
